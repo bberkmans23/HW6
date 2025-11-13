41776d1662dd6d73d0654eace85cfd74ae46fb4c
bberkmans23 <bberkmans@luc.edu>
2025-11-12 19:32:49 -0600

    Update author name in ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,7 +1,7 @@
 
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   brendan berkmans / 002
  *
  *   This java file contains the problem solutions for the methods lastBoulder,
  *   showDuplicates, and pair methods. You should utilize the Java Collection
